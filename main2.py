c3bb7848538e52517569264d6644caaf78a59459
ikuli <boyindaspace@gmail.com>
2025-11-16 16:36:44 +0500

    second bot

--- main2.py
+++ main2.py
@@ -42,20 +42,12 @@ class Vote(BaseModel):
 
 # 🔐 База для второго бота — РЕГЛАБ (или объединённая — см. примечание)
 def load_local_employees():
+    df = pd.read_excel("/root/voting_bot/reg_lab_staff.xlsx")
     employees = {}
-    # 1. Основной штат
-    df1 = pd.read_excel("/root/voting_bot/prosoft_staff.xlsx")
-    for _, row in df1.iterrows():
+    for _, row in df.iterrows():
         fio = str(row["ФИО"]).strip()
         dept = str(row["Подразделение"]).strip()
         if fio:
-            employees[fio] = dept
-    # 2. Реглаб
-    df2 = pd.read_excel("/root/voting_bot/reg_lab_staff.xlsx")
-    for _, row in df2.iterrows():
-        fio = str(row["ФИО"]).strip()
-        dept = str(row["Подразделение"]).strip()
-        if fio and fio not in employees:
             employees[fio] = dept
     return employees
 
